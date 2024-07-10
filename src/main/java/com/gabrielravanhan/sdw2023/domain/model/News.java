69d7efbe70506f6dfa5f84778e9d730d273f7a00
Gabriel Ravanhan <gabriel.ravanhan@outlook.com>
2024-07-10 20:37:56 -0300

    Implementação da camada de domínio da aplicação

---
+++ src/main/java/com/gabrielravanhan/sdw2023/domain/model/News.java
@@ -0,0 +1,7 @@
+package com.gabrielravanhan.sdw2023.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+}
---
+++ src/main/java/com/gabrielravanhan/sdw2023/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.gabrielravanhan.sdw2023.domain.repository;
+
+import com.gabrielravanhan.sdw2023.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
